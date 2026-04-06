47d3cc0d264c345b01641db8318a7f4d5f19b537
zakizafran <zakizafran96@gmail.com>
2026-04-06 10:49:12 +0700

    tambahan materi

---
+++ C.FungsiDanModularitas/LatihanFungsiDanModularitas/2_latihan_scope_variable.js
@@ -0,0 +1,42 @@
+//1.
+let kota = "jakarta";
+
+function infoWarga(){
+    let rahasiaWarga = " ada harta karun di monas";
+
+    console.log(rahasiaWarga);
+
+    return rahasiaWarga;
+}
+
+infoWarga();
+
+
+//2.
+let senjataku = "Pedang Kayu";
+
+function masukGua(){
+    
+    let senjataku = "ekskalibur";
+
+    console.log(senjataku);
+
+    return senjataku;
+}
+
+masukGua();
+
+console.log(senjataku);
+
+
+//2.
+
+if (true) {
+    var pesanRahasia = "Ssssstttt!"
+}
+
+console.log(pesanRahasia);
+
+
+
+
